cf0cf9aedce99739b30b20009c21cbf5e8585af2
sejung1997 <ksgve1997814@naver.com>
2022-01-15 23:26:13 +0900

    0115

---
+++ freeboard_frontend/pages/aaa/index.js
@@ -0,0 +1,69 @@
+
+// import {useRouter} from 'next/router'
+// import { useQuery, gql } from '@apollo/client'
+import { useState } from "react" 
+
+import {
+  Main,
+  Baner,
+  Name,
+  Title,
+  MainImg,
+  Contents,
+  Video,
+} from "../../styles/board"
+// const FETCH_BOARD = gql `
+//   query fetctBoard($number: Int) {
+//     fetchBoard(number: $number) {
+      
+//       writer
+//       title
+//       contents
+//       like
+//       createdAT
+
+//     }
+//   }
+
+// `
+
+export default function DynamicRoutedPage() {
+
+
+// const router = useRouter()
+// const {data} = useQuery(FETCH_BOARD, {
+//   variables: {number: Number(router.query.aaa)}
+// })
+
+//mutation은 대괄호,원하는 시점에 요청가능 query는 중괄호,페이지 실행될떄 자동으로
+
+
+
+
+// console.log(router.query.aaa)
+// console.log(data)
+
+  return (
+    <Main>
+      <Baner>
+        <img></img>
+        <Name> 이름:  Date:  </Name>
+        <img></img>
+        <img></img>
+      </Baner>
+      <Title>제목</Title>
+      <MainImg>1</MainImg>
+      <Contents>2</Contents>
+      <Video>2</Video>
+
+     
+
+
+      
+      
+    </Main>
+
+
+  
+ )
+}
---
+++ freeboard_frontend/styles/board.js
@@ -0,0 +1,46 @@
+import styled from "@emotion/styled"
+
+
+export const Main = styled.div `
+  width: 1200px;
+  margin-top: 300px;
+  margin-left: 100px;
+  box-shadow: 0px 4px 20px rgba(0,0,0,.2);
+  border: 1px, solid, #FFF;
+  font-family: sans-serif;
+  padding: 0 102px  80px
+
+
+`
+export const Name = styled.div `
+
+`
+export const Baner = styled.div `
+  height: 160px;
+  display: flex;
+  flex-direction: row;
+  border-bottom: 1px solid grey
+  
+`
+export const Title = styled.div `
+  font-size: 36px;
+  font-weight: bold;
+  padding-top: 80px;
+
+`
+export const MainImg = styled.div `
+  padding-top: 40px;
+  width: 996px;
+  height: 486px;
+
+
+`
+export const Contents = styled.div `
+  width: 996px;
+  height: 96px;
+
+
+`
+export const Video = styled.div `
+
+`
