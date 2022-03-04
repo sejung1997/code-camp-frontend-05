07ad71289ccad55904baccbf88a0c007e6bf6013
sejung1997 <ksgve1997814@naver.com>
2022-03-04 11:11:53 +0900

    dd

---
+++ class_build/next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  trailingSlash: true, // slash를 기준으로 폴더와 피일을 만들겠다
+};
+
+module.exports = nextConfig;
---
+++ class_build/pages/boards/[boardId]/index.js
@@ -0,0 +1,3 @@
+export default function BoardDetailPage() {
+  return <div>안녕하세요 상세페이지입니다</div>;
+}
