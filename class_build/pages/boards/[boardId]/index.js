dfd3f13f102845c3d7ebf3a7a296dbe9c11e3f11
sejung1997 <ksgve1997814@naver.com>
2022-03-07 12:28:35 +0900

    update

--- class_build/pages/boards/[boardId]/index.js
+++ class_build/pages/boards/[boardId]/index.js
@@ -1,9 +1,24 @@
 import { useRouter } from "next/router";
-
+import  Head  from "next/head";
 export default function BoardDetailPage() {
 
 
   const router= useRouter()
 
-  return <div>안녕하세요 상세페이지입니다,게시글아이디는{ router.query.boardId} 입니다</div>;
+  return (
+    <div>
+    <div>
+      <Head>
+        <meta property="og:title" content="나의 게시판"/>
+        <meta property="og:description" content="나의 게시판에 오신것을 환영합니다"/>
+        <meta property="og:image" content="https://dullyshin.github.io/2018/08/30/HTML-imgLink/#lg=1&slide=0"/>
+
+      </Head>
+    </div>
+    <div>
+       안녕하세요 상세페이지입니다,게시글아이디는{ router.query.boardId} 입니다
+    </div>
+  </div>
+  )
+ 
 }
