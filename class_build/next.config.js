b00808a998c9d05156f9273a7198ed7deb57700b
sejung1997 <ksgve1997814@naver.com>
2022-03-07 13:30:57 +0900

    dd

--- class_build/next.config.js
+++ class_build/next.config.js
@@ -2,6 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   trailingSlash: true, // slash를 기준으로 폴더와 피일을 만들겠다
+  exportPathMap: () => ({
+    "/": {page: "/"},
+    "/boards": {page: "/boards"},
+    "/404": {page: "/404"},
+  })
 };
 
 module.exports = nextConfig;
