e45f4c773b37d71534ca3bf64b67d5524e676031
sejung1997 <ksgve1997814@naver.com>
2022-03-13 23:42:54 +0900

    0313

--- market/pages/world/index.tsx
+++
@@ -1,6 +0,0 @@
-import FetchItemContainer from "../../src/units/List2/container";
-import { widthAuth } from "../../src/commons/withAuth";
-const WorldCupPage = () => {
-  return <FetchItemContainer />;
-};
-export default widthAuth(WorldCupPage);
