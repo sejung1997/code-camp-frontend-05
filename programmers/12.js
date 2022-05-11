04454fbeb4c8913bd2605ef90f734b57bcfea7e8
sejung1997 <ksgve1997814@naver.com>
2022-05-11 23:21:45 +0900

    sd

---
+++ programmers/12.js
@@ -0,0 +1,21 @@
+function solution(s) {
+    
+  const splitS = s.split("")
+  const abbreviation = (number) => {
+      const answer = numeber;
+      for(let i = number * 2; i< splitS.number - number * 2 + (number ===1? 1: 0); i++) {
+          
+      }
+  } 
+  for(let i = 1; i <= s.length/2 i ++) {
+      for(let j = 0; j < i; j++) {
+          if(splitS[j] !== splitS[j+i]) break
+          if(j === i - 1) {
+              
+          }
+          
+      }
+  }
+ 
+  return answer;
+}
