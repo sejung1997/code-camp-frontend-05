f9b7fe5aacf1daeaf4019e8fcac32ad2b4724fa8
sejung1997 <ksgve1997814@naver.com>
2022-05-13 00:09:29 +0900

    0513

--- programmers/12.js
+++ programmers/12.js
@@ -19,3 +19,39 @@ function solution(s) {
  
   return answer;
 }
+
+function solution(skill, skill_trees) {
+    const isTrue = (arr) => {
+        if(skill.length === 1) return true
+        let num = new Array(skill.length)
+        num[0] = arr.indexOf(skill[0])
+        if(num[0] === - 1) num[0] = arr.length
+        console.log(num)
+        
+        for(let i = 1; i < skill.length; i++) {
+             num[i] = arr.indexOf(skill[i])
+             if(num[i] === - 1) num[i] = i + arr.length
+             
+             if(num[i - 1] > num[i]) {
+                 break
+                 return false
+                 
+             }
+              if(i === skill.length - 1 ) return true
+         }
+         
+    }
+    
+     return skill_trees.filter(x => isTrue(x)).length
+ }
+ function solution(d, budget) {
+    var answer = 0;
+    let i = 0
+    const a = d.sort((a,b)=>(a-b))
+    while( budget - a[i] >= 0 && i <= d.length) {
+        budget -= a[i]
+        answer += 1 
+        i +=1
+    }
+    return answer
+}
